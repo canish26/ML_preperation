9df02ab94d01002b58613ff7e9fea63cab447873
Anish Chintamaneni <112290448+canish26@users.noreply.github.com>
2024-11-26 21:37:02 -0800

    Update convolution_neural_network.py

--- convolution_neural_network.py
+++ convolution_neural_network.py
@@ -1,18 +1,3 @@
-"""
-     - - - - - -- - - - - - - - - - - - - - - - - - - - - - -
-    Name - - CNN - Convolution Neural Network For Photo Recognizing
-    Goal - - Recognize Handing Writing Word Photo
-    Detail：Total 5 layers neural network
-            * Convolution layer
-            * Pooling layer
-            * Input layer layer of BP
-            * Hidden layer of BP
-            * Output layer of BP
-    Author: Stephen Lee
-    Github: 245885195@qq.com
-    Date: 2017.9.20
-    - - - - - -- - - - - - - - - - - - - - - - - - - - - - -
-"""
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
