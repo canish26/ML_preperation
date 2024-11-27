cd646509f776efd20d786ee3445adb950ae38c2a
Anish Chintamaneni <112290448+canish26@users.noreply.github.com>
2024-11-26 21:37:23 -0800

    Update IntroductiontoRegexModule.py

--- RegexandParsing/IntroductiontoRegexModule.py
+++ RegexandParsing/IntroductiontoRegexModule.py
@@ -1,11 +1,3 @@
-'''
-Title     : Introduction to Regex Module
-Subdomain : Regex and Parsing
-Domain    : Python
-Author    : Ahmedur Rahman Shovon
-Created   : 15 July 2016
-Problem   : https://www.hackerrank.com/challenges/introduction-to-regex/problem
-'''
 import re
 n = int(input())
 for i in range(n):
